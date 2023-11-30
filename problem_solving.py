aa3bafd20bbf4cccf876f629230ebe339336360b
Gero Zayas <gerozayas@gmail.com>
2023-11-30 09:59:38 +0100

    Fix Layout and txt download

--- problem_solving.py
+++ problem_solving.py
@@ -1,24 +1,19 @@
 import PySimpleGUI as sg
 
-# Define the layout
-layout = [
-    [sg.Text("PROBLEM-SOLVING QUESTIONS from Tony Robbins")],
-    [sg.Text("1. What is great about this problem?")],
-    [sg.Multiline(size=(None, 2), key="Q1")],  # Two lines high input box
-    [sg.Text("2. What is not perfect yet?")],
-    [sg.Multiline(size=(None, 2), key="Q2")],
-    [sg.Text("3. What am I willing to do to make it the way I want it?")],
-    [sg.Multiline(size=(None, 2), key="Q3")],
-    [sg.Text("4. What am I willing to no longer do to make it the way I want it?")],
-    [sg.Multiline(size=(None, 2), key="Q4")],
-    [
-        sg.Text(
-            "5. How can I enjoy the process while I do what is necessary to make it the way I want it?"
-        )
-    ],
-    [sg.Multiline(size=(None, 2), key="Q5")],
-    [sg.Button("Submit"), sg.Button("Save Responses")],
+# Define the questions
+questions = [
+    "1. What is great about this problem?",
+    "2. What is not perfect yet?",
+    "3. What am I willing to do to make it the way I want it?",
+    "4. What am I willing to no longer do to make it the way I want it?",
+    "5. How can I enjoy the process while I do what is necessary to make it the way I want it?",
 ]
+
+# Define the layout
+layout = [[sg.Text("PROBLEM-SOLVING QUESTIONS from Tony Robbins")]]
+for i, question in enumerate(questions, 1):
+    layout += [[sg.Text(question)], [sg.Multiline(size=(None, 2), key=f"Q{i}")]]
+layout += [[sg.Button("Submit"), sg.Button("Save Responses")]]
 
 # Create the Window
 window = sg.Window("Survey Application", layout)
@@ -35,10 +30,9 @@ while True:
         sg.popup("Responses recorded!")
     elif event == "Save Responses":
         with open("responses.txt", "w") as file:
-            for question, response in responses.items():
-                file.write(
-                    f"{question}: {response.strip()}\n"
-                )  # strip to remove extra newlines
+            for i, question in enumerate(questions, 1):
+                response = responses.get(f"Q{i}", "").strip()
+                file.write(f"{question}\n{response}\n\n")
         sg.popup("Responses saved to responses.txt!")
 
 window.close()
